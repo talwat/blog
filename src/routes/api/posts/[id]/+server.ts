1dd704c40cb15c753a100eb3d0139e541d6dbd62
talwat <talrac1020@gmail.com>
2022-09-09 13:55:36 +0200

    Formatted code

--- src/routes/api/posts/[id]/+server.ts
+++ src/routes/api/posts/[id]/+server.ts
@@ -41,8 +41,8 @@ export const GET: RequestHandler = async ({ params }) => {
       if (!ids) return false;
 
       const sections = level === 1 ? "" : "</section><section>"; // Close previous section and open a new one
-      const headingAnchorImage = `<img class="heading-anchor-image" src="/blog/svg/link.svg">`
-      const headingAnchorText = `<h${level} id="${ids[1]}">${textWithoutID}</h${level}>`
+      const headingAnchorImage = `<img class="heading-anchor-image" src="/blog/svg/link.svg">`;
+      const headingAnchorText = `<h${level} id="${ids[1]}">${textWithoutID}</h${level}>`;
       const anchor = `<a class="heading-anchor" href="/blog/posts/${params.id}#${ids[1]}">${headingAnchorImage}${headingAnchorText}</a>`;
 
       return `${sections} ${anchor}`;
