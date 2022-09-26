8d1f240b826a8ce222f887c6377c9e6b4a117fce
talwat <talrac1020@gmail.com>
2022-09-26 20:31:51 +0200

    Leave old 404 page as is

--- src/routes/404/+page.svelte
+++ src/routes/404/+page.svelte
@@ -1,1 +1,7 @@
-test
+<script>
+  import { page } from "$app/stores";
+  import Error from "$lib/Error.svelte";
+</script>
+
+<!-- Specify status, but leave everything else -->
+<Error page={{ ...$page, status: 404 }} />
