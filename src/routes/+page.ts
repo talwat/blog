d9e16959a045e65f60ffb38078a40c24d1cac4bc
talwat <talrac1020@gmail.com>
2022-09-26 18:54:45 +0200

    Fix list api endpoint

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,7 +2,7 @@ import type { DocAttributes } from "src/docs";
 import type { PageLoad } from "./$types";
 
 export const load: PageLoad = async ({ fetch }) => {
-  const resp = await fetch(`/blog/api/docs/posts/list`);
+  const resp = await fetch(`/api/docs/posts/list`);
   const json: DocAttributes[] = (await resp.json()) as DocAttributes[];
 
   return {
